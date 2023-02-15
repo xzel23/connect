d4662bfc63f4d8f5dd1bc7a246ca4e126dbcca0a
Axel Howind <axh@dua3.com>
2023-02-15 16:48:38 +0100

    fix whitespace

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@
  */
 module com.dua3.connect {
     exports com.dua3.connect;
-    
+
     requires org.apache.httpcomponents.client5.httpclient5;
     requires org.apache.httpcomponents.client5.httpclient5.win;
     requires org.slf4j;
